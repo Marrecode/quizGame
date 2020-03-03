195475b45c8bc81bff5c58b2a012913c29fd97a5
Fredrikwide <Fredrik.widegren@gmail.com>
2020-03-03 15:54:06 +0100

    added cities collection to firebase

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import './App.css';
 import {db } from './modules/firebase';
 
 db.collection("music_quiz").get().then(function(querySnapshot) {
@@ -7,6 +6,34 @@ db.collection("music_quiz").get().then(function(querySnapshot) {
       console.log(doc.id, " => ", doc.data());
   });
 });
+
+const postData = () => {
+  const citiesRef = db.collection("cities");
+
+citiesRef.doc("SF").set({
+    name: "San Francisco", state: "CA", country: "USA",
+    capital: false, population: 860000,
+    regions: ["west_coast", "norcal"] });
+citiesRef.doc("LA").set({
+    name: "Los Angeles", state: "CA", country: "USA",
+    capital: false, population: 3900000,
+    regions: ["west_coast", "socal"] });
+citiesRef.doc("DC").set({
+    name: "Washington, D.C.", state: null, country: "USA",
+    capital: true, population: 680000,
+    regions: ["east_coast"] });
+citiesRef.doc("TOK").set({
+    name: "Tokyo", state: null, country: "Japan",
+    capital: true, population: 9000000,
+    regions: ["kanto", "honshu"] });
+citiesRef.doc("BJ").set({
+    name: "Beijing", state: null, country: "China",
+    capital: true, population: 21500000,
+    regions: ["jingjinji", "hebei"] });
+
+}
+
+postData();
 
 
 function App() {
