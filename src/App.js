0ee3bf5d9ff06564377f2987f09441e091c567fd
Martin Gustavsson <Marreeeehh93@gmail.com>
2020-03-03 15:42:37 +0100

    yes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import React from 'react';
 import './App.css';
+import {db } from './modules/firebase';
+
+/*db.collection("music_quiz").get().then((querySnapshot) => {
+  querySnapshot.forEach((doc) => {
+      console.log(`${doc.id} => ${doc.data()}`);
+  });
+});*/
+
+db.collection("music_quiz").get().then(function(querySnapshot) {
+  querySnapshot.forEach(function(doc) {
+      // doc.data() is never undefined for query doc snapshots
+      console.log(doc.id, " => ", doc.data());
+  });
+});
+
+
+
+
+
 
 function App() {
   return (
     <div className="App">
-     <p>hello</p>
+      <h1>Hej</h1>
+      <h2>DÅ</h2>
     </div>
   );
 }
