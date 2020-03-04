df94c05d1a312f4cbf8bffe6e204fbd56817c29e
Jasmine Silfver <jasmine.silfver@gmail.com>
2020-03-04 14:23:12 +0100

    QG4-22 Changing to making Quiz component instead

--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
 import React from 'react';
-import {db } from './modules/firebase';
-
-db.collection("music_quiz").get().then(function(querySnapshot) {
-  querySnapshot.forEach(function(doc) {
-      console.log(doc.id, " => ", doc.data());
-  });
-});
 
 
 function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+	return (
+		<div className="App">
+		</div>
+	);
 }
 
 export default App;
