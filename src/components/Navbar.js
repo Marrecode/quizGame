f78fd2e0a847431863761c04591dbaf689fb026d
Jasmine Silfver <jasmine.silfver@gmail.com>
2020-03-06 15:18:18 +0100

    QG4-9 Done some displaying the points stuff

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,12 @@
 import React from 'react'
-// import {NavLink} from 'react-router-dom'
+import {Link, NavLink} from 'react-router-dom'
 
 const Navbar = () => {
 
     return(
         <nav className="navbar navbar-expand-lg navbar-light bg-light mb-5">
             <div className="container">
-                <a className="navbar-brand" href="#">The Awesome Quizzes</a>
+            <Link to="/" className="navbar-brand">The Awesome Quizzes</Link>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
