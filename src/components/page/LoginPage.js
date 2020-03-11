7d518ae3c19303b6421162a353de37d3e1095350
Martin Gustavsson <Marreeeehh93@gmail.com>
2020-03-11 10:39:55 +0100

    fix some cssStyling

--- src/components/page/LoginPage.js
+++ src/components/page/LoginPage.js
@@ -40,9 +40,9 @@ class LoginPage extends React.Component {
 
         render() {
             return (
-            <div id="login">
+            <div className="login">
                 <h1>Login</h1>
-            <form id="login-form" onSubmit={this.formSubmit} >
+            <form id="login" className="login" onSubmit={this.formSubmit} >
                 <div className="form-group">
                     <label htmlFor="email">Email</label>
                     <input type="email" id="email" className="form-control" onChange={this.formOnChange}/>
@@ -52,8 +52,10 @@ class LoginPage extends React.Component {
                     <label htmlFor="password">Password</label>
                     <input type="password" id="password" className="form-control" onChange={this.formOnChange}/>
                 </div>
+                <div className="buttons">
                 <button type="submit" className="btn btn-primary">Log in</button>
-                <Link onClick={this.signUp} class="btn btn-success">Sign Up</Link>            
+                <Link onClick={this.signUp} class="btn btn-success">Sign Up</Link>
+                </div>            
                 </form>
                 
             </div>
